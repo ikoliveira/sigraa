76873d26a4c759a6738c35ccc7412950d12a44cf
ikoliveira <igor.oliveira@ccc.ufcg.edu.br>
2025-02-16 02:19:47 -0300

    ✶ feat: add a classe which represents a Person in the system

---
+++ models/Pessoa.java
@@ -0,0 +1,60 @@
+package models;
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+    private ArrayList<Contato> contatos;
+
+    public Pessoa(String nome, int idade) {
+        this.setNome(nome);
+        this.setIdade(idade);
+        this.contatos = new ArrayList<Contato>();  
+    }
+
+    public String getNome() { return this.nome; }
+    private void setNome(String nome){
+
+        this.nome = nome;
+
+    }
+
+    public int getIdade() { return this.idade; }
+    private void setIdade(int idade){
+        
+        this.idade = idade;
+
+
+    }
+
+
+    public List<Contato> getContatos() { return this.contatos; }
+    public void addContato(String tipo, String valor ) {
+
+        Contato novoContato = new Contato(tipo, valor);
+        getContatos().add(novoContato);
+    
+    }
+
+    /**
+     * itera o arraylist procurando o contato certo, em seguida remove-o
+     * @param valor representa o e-mail ou telefone do contato
+     */
+    public void rmContato(String valor) {
+
+
+        ArrayList<Contato> contatos = this.getContatos();
+
+        for (int i = 0; i < contatos.size(); i++) {
+
+            if(contatos.get(i).getContato().equals(valor)) {
+                contatos.remove(i);
+                break;
+            }
+
+        }
+
+    }
+
+
+
+}
